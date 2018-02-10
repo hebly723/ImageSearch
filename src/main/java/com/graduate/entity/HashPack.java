3de2e4c9fa25149a7f8a06e2b15df7a0d040c5bd
hebly723 <hebly723@163.com>
2018-02-11 00:10:01 +0800

    颜色向量角算法中加强图片信息提取的精确性，准确生成97.6%的hash码

---
+++ src/main/java/com/graduate/entity/HashPack.java
@@ -0,0 +1,23 @@
+package com.graduate.entity;
+
+public class HashPack {
+    private String hashCva;
+    private String hashPha;
+
+    public String getHashCva() {
+        return hashCva;
+    }
+
+    public void setHashCva(String hashCva) {
+        this.hashCva = hashCva;
+    }
+
+    public String getHashPha() {
+        return hashPha;
+    }
+
+    public void setHashPha(String hashPha) {
+        this.hashPha = hashPha;
+    }
+
+}
